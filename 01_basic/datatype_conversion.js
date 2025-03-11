5056ef00462fad06eca0ffa8a769084a5baf862d
Aakashpatel01 <156766819+Aakashpatel01@users.noreply.github.com>
2025-03-11 04:37:14 +0000

    sum codes

---
+++ 01_basic/datatype_conversion.js
@@ -0,0 +1,6 @@
+let number = 33;
+let num_string = String(number);
+typeof(number);
+console.log(typeof(number));
+console.log(typeof(num_string));
+
---
+++ 01_basic/date.js
@@ -0,0 +1,4 @@
+let Mydate = new Date();
+console.log(Mydate);
+console.log(Mydate.toDateString());
+console.log(Mydate.toLocaleString());
