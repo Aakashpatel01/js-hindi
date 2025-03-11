9e10ae2775f0d4e8c7c869f1afd5dd9fdb88f68e
Aakashpatel01 <156766819+Aakashpatel01@users.noreply.github.com>
2025-03-11 10:17:17 +0000

    two or added i java seriece

---
+++ 01_basic/arrow_functios.js
@@ -0,0 +1,14 @@
+let arrowtexting = (num1, num2) => {
+
+    if(num1 > num2){
+        num3 = num1+num2;
+        console.log(num3);
+        
+        
+    }else{
+        console.log("sum time to execute this code");
+        
+    }
+
+}
+arrowtexting(5, 3)
---
+++ 01_basic/foreach_map_filter.js
@@ -0,0 +1,23 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// arr.forEach( (num) =>  { 
+//     if(num > 4) 
+// console.log(num + 10);
+
+// })
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+arr.filter((num) => {
+
+    if(num >4){
+        
+        num = num *2;
+        console.log(num);
+        
+        
+    }
+
+})
+    
+
